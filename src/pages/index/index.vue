3c8e873f91cf9bace18e4e2e30afafd307ec316c
骆沛 <luopei@11.com>
2022-05-07 14:23:37 +0800

    feat: 完善目录结构和完成首页布局

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,41 +1,414 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
+  <view class="demo">
+    <view class="demo-item" v-for="(item, index) in list" :key="index">
+      <view class="demo-title">{{ item.title }}</view>
+      <view class="demo-content" v-for="(item1, index1) in item.item" :key="index1" @click="clickItem(item1)">
+        <view class="demo-content-left">
+          <view class="demo-content-left-label">{{ item1.label }}</view>
+          <view class="demo-content-left-title">{{ item1.title }}</view>
+        </view>
+        <view class="demo-content-icon">
+          <cc-icon type="arrowright" size="22" color="#979797"></cc-icon>
+        </view>
+      </view>
     </view>
   </view>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
+interface Item {
+  label: string
+  title: string
+  url: string
+}
+interface ListItem {
+  title: string
+  item: Item[]
+}
+const list: ListItem[] = [
+  {
+    title: '基础组件',
+    item: [
+      {
+        label: 'Button',
+        title: '按钮',
+        url: '/button',
+      },
+      {
+        label: 'Cell',
+        title: '单元格',
+        url: '/cell',
+      },
+      {
+        label: 'Layout',
+        title: '布局',
+        url: '/layout',
+      },
+      {
+        label: 'Popup',
+        title: '弹出层',
+        url: '/popup',
+      },
+      {
+        label: 'Toast',
+        title: '轻提示',
+        url: '/toast',
+      },
+    ],
+  },
+  {
+    title: '表单组件',
+    item: [
+      {
+        title: 'Calendar',
+        label: '日历',
+        url: '/calendar',
+      },
+      {
+        title: 'Checkbox',
+        label: '复选框',
+        url: '/checkbox',
+      },
+      {
+        title: 'Filed',
+        label: '输入框',
+        url: '/field',
+      },
+      {
+        title: 'Form',
+        label: '表单',
+        url: '/form',
+      },
+      {
+        title: 'NumberKeyboard',
+        label: '数字键盘',
+        url: '/numberKeyboard',
+      },
+      {
+        title: 'PasswordInput',
+        label: '密码输入框',
+        url: '/passwordInput',
+      },
+      {
+        title: 'Radio',
+        label: '单选框',
+        url: '/radio',
+      },
+      {
+        title: 'Rate',
+        label: '评分',
+        url: '/rate',
+      },
+      {
+        title: 'Search',
+        label: '搜索框',
+        url: '/search',
+      },
+      {
+        title: 'Slider',
+        label: '滑块',
+        url: '/slider',
+      },
+      {
+        title: 'Stepper',
+        label: '步进器',
+        url: '/stepper',
+      },
+      {
+        title: 'Switch',
+        label: '开关',
+        url: '/switch',
+      },
+      {
+        title: 'Checker',
+        label: '选择项',
+        url: '/checker',
+      },
+      {
+        title: 'Upload',
+        label: '文件上传',
+        url: '/upload',
+      },
+    ],
+  },
+  {
+    title: '反馈组件',
+    item: [
+      {
+        title: 'ActionSheet',
+        label: '动作面板',
+        url: '/actionSheet',
+      },
+      {
+        title: 'Dialog',
+        label: '弹出框',
+        url: '/dialog',
+      },
+      {
+        title: 'DropdownMenu',
+        label: '下拉菜单',
+        url: '/dropdown',
+      },
+      {
+        title: 'Loading',
+        label: '加载',
+        url: '/loading',
+      },
+      {
+        title: 'Notify',
+        label: '消息通知',
+        url: '/notify',
+      },
+      {
+        title: 'Mask',
+        label: '遮罩层',
+        url: '/mask',
+      },
+      {
+        title: 'PullRefresh',
+        label: '下拉刷新',
+        url: '/pullRefresh',
+      },
+      {
+        title: 'SwipeCell',
+        label: '滑动单元格',
+        url: '/swipeCell',
+      },
+      {
+        title: 'Avatar',
+        label: '头像',
+        url: '/avatar',
+      },
+    ],
+  },
+  {
+    title: '展示组件',
+    item: [
+      {
+        title: 'Badge',
+        label: '徽标',
+        url: '/badge',
+      },
+      {
+        title: 'CircleProgress',
+        label: '环形进度条',
+        url: '/circleProgress',
+      },
+      {
+        title: 'Collapse',
+        label: '折叠面板',
+        url: '/collapse',
+      },
+      {
+        title: 'CountDown',
+        label: '倒计时',
+        url: '/countDown',
+      },
+      {
+        title: 'Countup',
+        label: '数字滚动',
+        url: '/countup',
+      },
+      {
+        title: 'viewider',
+        label: '分割线',
+        url: '/viewider',
+      },
+      {
+        title: 'ImagePreview',
+        label: '图片预览',
+        url: '/imagePreview',
+      },
+      {
+        title: 'NoticeBar',
+        label: '通知栏',
+        url: '/noticeBar',
+      },
+      {
+        title: 'Progress',
+        label: '进度条',
+        url: '/progress',
+      },
+      {
+        title: 'Skeleton',
+        label: '骨架屏',
+        url: '/skeleton',
+      },
+      {
+        title: 'Steps',
+        label: '步骤条',
+        url: '/steps',
+      },
+      {
+        title: 'Sticky',
+        label: '粘性布局',
+        url: '/sticky',
+      },
+      {
+        title: 'Swiper',
+        label: '轮播',
+        url: '/swiper',
+      },
+      {
+        title: 'Tag',
+        label: '标签',
+        url: '/tag',
+      },
+      {
+        title: 'OpenMore',
+        label: '展开更多',
+        url: '/openMore',
+      },
+      {
+        title: 'LoadMore',
+        label: '加载更多',
+        url: '/loadMore',
+      },
+      {
+        title: 'Gap',
+        label: '间隔槽',
+        url: '/gap',
+      },
+    ],
+  },
+  {
+    title: '导航组件',
+    item: [
+      {
+        title: 'Grid',
+        label: '宫格',
+        url: '/grid',
+      },
+      {
+        title: 'IndexBar',
+        label: '索引栏',
+        url: '/indexBar',
+      },
+      {
+        title: 'NavBar',
+        label: '导航栏',
+        url: '/navBar',
+      },
+      {
+        title: 'Sidebar',
+        label: '侧边导航',
+        url: '/sidebar',
+      },
+      {
+        title: 'Tab',
+        label: '标签页',
+        url: '/tabs',
+      },
+      {
+        title: 'Tabbar',
+        label: '标签栏',
+        url: '/tabbar',
+      },
+      {
+        title: 'Link',
+        label: '超链接',
+        url: '/link',
+      },
+      {
+        title: 'TreeSelect',
+        label: '分类选择',
+        url: '/treeSelect',
+      },
+    ],
+  },
+  {
+    title: '业务组件',
+    item: [
+      {
+        title: 'AddressEdit',
+        label: '地址编辑',
+        url: '/addressEdit',
+      },
+      {
+        title: 'AddressList',
+        label: '地址列表',
+        url: '/addressList',
+      },
+      // {
+      //   title: 'Area',
+      //   label: '省市区选择',
+      //   url: '/area'
+      // },
+      {
+        title: 'goodsCard',
+        label: '商品卡片',
+        url: '/goodsCard',
+      },
+      {
+        title: 'contactCard',
+        label: '联系人卡片',
+        url: '/contactCard',
+      },
+      {
+        title: 'contactList',
+        label: '联系人列表',
+        url: '/contactList',
+      },
+      {
+        title: 'GoodsAction',
+        label: '商品导航',
+        url: '/goodsAction',
+      },
+      {
+        title: 'SubmitBar',
+        label: '提交订单栏',
+        url: '/submitBar',
+      },
+      {
+        title: 'Coupon',
+        label: '优惠券',
+        url: '/coupon',
+      },
+    ],
+  },
+]
+let clickItem = (item: Item) => {
+  uni.navigateTo({
+    url: item.url,
+  })
+}
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+<style lang="scss" scoped>
+.demo {
+  background: #f7f8fa;
+  padding: 60rpx 50rpx;
+  &-title {
+    font-size: 14px;
+    color: #909ca4;
+    margin-bottom: 40rpx;
+  }
+  &-content {
+    position: relative;
+    display: flex;
+    align-items: center;
+    padding: 0 80rpx;
+    background: #fff;
+    height: 90rpx;
+    background: #fff;
+    border-radius: 44rpx;
+    box-shadow: 0 1px 8rpx 0 rgb(102 102 102 / 6%);
+    margin-bottom: 26rpx;
+    &-left {
+      display: flex;
+      align-items: center;
+      font-weight: 700;
+      color: #333;
+      &-title {
+        margin-left: 20rpx;
+      }
+    }
+    &-icon {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      right: 60rpx;
+    }
+  }
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
-import uni from "@dcloudio/vite-plugin-uni";
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
-});
+  server: {
+    port: 9527,
+  },
+})
