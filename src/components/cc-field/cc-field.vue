7a441f6b4a45b5045800dcb78eb9b52ac855757a
骆沛 <luopei@11.com>
2023-08-25 16:07:10 +0800

    feat(custom): 完成field组件

---
+++ src/components/cc-field/cc-field.vue
@@ -0,0 +1,230 @@
+<template>
+  <view class="cc-field cc-filed-cell-left-icon" :class="{ 'cc-field-start': !$slots.rightIcon }">
+    <cc-cell :border="border" :icon="leftIcon">
+      <template #title>
+        <view
+          v-if="label"
+          @click="clickLeft"
+          class="cc-field-label"
+          :class="{ 'cc-field-label-disabled': disabled }"
+        >
+          <view v-if="required" class="cc-field-label-required">*</view>
+          <view>{{ label }}</view>
+        </view>
+      </template>
+      <template #value>
+        <view style="width: 100%; display: flex; align-items: center">
+          <textarea
+            v-model="inputValue"
+            class="cc-field-input"
+            v-if="type === 'textarea'"
+            :style="{ height: 24 * Number(rows) + 'rpx' }"
+            :maxlength="maxlength"
+          ></textarea>
+          <input
+            v-else
+            class="cc-field-input"
+            :class="{
+              'cc-field-input-disabled': disabled,
+              'cc-field-input-error': error,
+            }"
+            v-model="inputValue"
+            :placeholder="placeholder"
+            :type="type"
+            :disabled="disabled"
+            :maxlength="Number(maxlength)"
+            @input="handleInput"
+            @change="handleChange"
+            @focus="handleFocus"
+            @blur="handleBlur"
+            @confirm="handleInput"
+          />
+          <view style="width: 100%" v-if="$slots.button">
+            <slot name="button"></slot>
+          </view>
+        </view>
+      </template>
+      <template #right-icon>
+        <view style="z-index: 999">
+          <cc-icon
+            @click="clear"
+            v-if="inputValue && clearable"
+            type="close"
+            size="16"
+            color="#969799"
+          ></cc-icon>
+          <cc-icon @click="clickRight" :type="rightIcon" size="16" color="#969799"></cc-icon>
+        </view>
+      </template>
+    </cc-cell>
+    <view class="cc-field-error-message">{{ errorMessage }}</view>
+    <view v-if="maxlength && showWordLimit" class="cc-field-word-limit"
+      >{{ inputValue.length }} / {{ maxlength }}</view
+    >
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+
+type FieldTypeProps = 'text' | 'tel' | 'password' | 'number' | 'textarea'
+
+const props = withDefaults(
+  defineProps<{
+    modelValue: string
+    label?: string
+    placeholder?: string
+    border?: boolean
+    clearable?: boolean
+    disabled?: boolean
+    required?: boolean
+    error?: boolean
+    showWordLimit?: boolean
+    type?: FieldTypeProps
+    leftIcon?: string
+    rightIcon?: string
+    errorMessage?: string
+    maxlength?: string | number
+    rows?: string | number
+  }>(),
+  {
+    modelValue: '',
+    label: '',
+    placeholder: '',
+    border: true,
+    clearable: false,
+    disabled: false,
+    required: false,
+    error: false,
+    showWordLimit: false,
+    type: 'text',
+    leftIcon: '',
+    rightIcon: '',
+    errorMessage: '',
+    maxlength: -1,
+    rows: 1,
+  }
+)
+
+const emits = defineEmits<{
+  'update:modelValue': [val: string]
+  change: [val: string]
+  input: [val: string]
+  focus: [e: FocusEvent]
+  blur: [e: FocusEvent]
+  clickRight: [e: Event]
+  clickLeft: [e: Event]
+  clear: [e: Event]
+  click: [e: Event]
+}>()
+
+const inputValue = ref<string>(props.modelValue)
+
+// 事件处理
+const clear = (e: Event) => {
+  inputValue.value = ''
+  emits('clear', e)
+}
+const handleInput = () => {
+  emits('input', inputValue.value)
+}
+const handleChange = () => {
+  emits('change', inputValue.value)
+}
+const handleFocus = (e: FocusEvent) => {
+  emits('focus', e)
+}
+const handleBlur = (e: FocusEvent) => {
+  emits('blur', e)
+}
+const clickLeft = (e: Event) => {
+  emits('clickLeft', e)
+}
+const clickRight = (e: Event) => {
+  emits('clickRight', e)
+}
+
+watch(
+  () => inputValue.value,
+  val => {
+    emits('update:modelValue', val)
+  }
+)
+watch(
+  () => props.modelValue,
+  val => {
+    inputValue.value = val
+  }
+)
+</script>
+
+<style scoped lang="scss">
+.cc-field {
+  position: relative;
+  width: 100%;
+  textarea {
+    position: relative;
+    top: 8rpx;
+  }
+  &-icon {
+    position: relative;
+    top: 4rpx;
+  }
+  &-error-message {
+    position: absolute;
+    color: #ee0a24;
+    font-size: 12px;
+    bottom: -40rpx;
+    left: 164rpx;
+  }
+  &-word-limit {
+    position: absolute;
+    color: #646566;
+    font-size: 12px;
+    bottom: -36rpx;
+    right: 0;
+  }
+  &-start {
+    .cc-cell-value {
+      justify-content: flex-start;
+    }
+  }
+  &-label {
+    min-width: 108rpx;
+    margin-right: 24rpx;
+    position: relative;
+    top: 4rpx;
+    &-disabled {
+      color: #c8c9cc;
+    }
+    &-required {
+      position: absolute;
+      left: -16rpx;
+      top: -4rpx;
+      color: #ee0a24;
+      font-size: 14px;
+    }
+  }
+  &-input {
+    display: block;
+    box-sizing: border-box;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    color: #323233;
+    line-height: inherit;
+    text-align: left;
+    border: 0;
+    resize: none;
+    outline: none;
+    &-error {
+      color: #ee0a24;
+    }
+    &-disabled {
+      color: #c8c9cc;
+      cursor: not-allowed;
+      opacity: 1;
+    }
+  }
+}
+</style>
---
+++ src/pages/field/index.vue
@@ -0,0 +1,165 @@
+<template>
+  <div style="background: #f7f7f7; height: 100vh">
+    <div class="demo">
+      <div class="demo-title">基础用法</div>
+      <div class="demo-item">
+        <cc-field
+          v-model="value1"
+          label="文本"
+          :border="false"
+          clearable
+          @clear="handleClear"
+          @input="handleInput"
+          @change="handleChange"
+          @focus="handleFocus"
+          @blur="handleBlur"
+          @clickLeft="clickLeft"
+        ></cc-field>
+      </div>
+      <div v-if="value1">当前输入框值: {{ value1 }}</div>
+    </div>
+
+    <div class="demo">
+      <div class="demo-title">输入框类型</div>
+      <div class="demo-item">
+        <cc-field v-model="value2" label="文本"></cc-field>
+      </div>
+      <div class="demo-item">
+        <cc-field v-model="value3" label="密码" type="password"></cc-field>
+      </div>
+      <div class="demo-item">
+        <cc-field v-model="value4" type="tel" label="手机号"></cc-field>
+      </div>
+      <div class="demo-item">
+        <cc-field v-model="value5" type="number" label="数字"></cc-field>
+      </div>
+    </div>
+
+    <div class="demo">
+      <div class="demo-title">禁用状态</div>
+      <div class="demo-item">
+        <cc-field v-model="value7" label="禁用" disabled></cc-field>
+      </div>
+    </div>
+
+    <div class="demo">
+      <div class="demo-title">显示图标</div>
+      <div class="demo-item">
+        <cc-field
+          v-model="value8"
+          left-icon="search"
+          right-icon="person"
+          placeholder="显示图标"
+          label="文本"
+          @clickRight="clickRight"
+        ></cc-field>
+      </div>
+      <div class="demo-item">
+        <cc-field left-icon="chat" v-model="value9" placeholder="显示图标" label="文本"></cc-field>
+      </div>
+    </div>
+
+    <div class="demo">
+      <div class="demo-title">必填</div>
+      <div class="demo-item">
+        <cc-field v-model="value10" label="文本" required></cc-field>
+      </div>
+      <div class="demo-item">
+        <cc-field
+          v-model="value11"
+          label="文本"
+          required
+          error
+          error-message="都怪那天雨下的太急"
+        ></cc-field>
+      </div>
+    </div>
+
+    <div class="demo">
+      <div class="demo-title">输入长度限制</div>
+      <div class="demo-item">
+        <cc-field :maxlength="50" show-word-limit v-model="value12" label="文本"></cc-field>
+      </div>
+    </div>
+
+    <div class="demo">
+      <div class="demo-title">自适应高度</div>
+      <div class="demo-item">
+        <cc-field
+          :maxlength="200"
+          show-word-limit
+          v-model="value13"
+          type="textarea"
+          rows="1"
+          label="文本"
+        ></cc-field>
+      </div>
+    </div>
+
+    <div class="demo">
+      <div class="demo-title">插入按钮</div>
+      <div class="demo-item">
+        <cc-field v-model="value14" label="文本">
+          <template #button>
+            <cc-button type="success" size="small">发送验证码</cc-button>
+          </template>
+        </cc-field>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const value1 = ref<string>('')
+const value2 = ref<string>('')
+const value3 = ref<string>('')
+const value4 = ref<string>('')
+const value5 = ref<string>('')
+const value6 = ref<string>('只读状态')
+const value7 = ref<string>('禁用状态')
+const value8 = ref<string>('')
+const value9 = ref<string>('123')
+const value10 = ref<string>('')
+const value11 = ref<string>('遇见你的那天起')
+const value12 = ref<string>('')
+const value13 = ref<string>('')
+const value14 = ref<string>('')
+
+const handleInput = (val: string) => {
+  console.log(val)
+}
+const handleChange = (val: string) => {
+  console.log(val)
+}
+const handleFocus = (e: FocusEvent) => {
+  console.log('focus', e)
+}
+const handleBlur = (e: FocusEvent) => {
+  console.log('blur', e)
+}
+const handleClear = (e: MouseEvent) => {
+  console.log('clear')
+}
+const clickLeft = (e: MouseEvent) => {
+  console.log('clickLeft', e)
+}
+const clickRight = (e: MouseEvent) => {
+  console.log('clickRight', e)
+}
+</script>
+
+<style scoped lang="scss">
+.demo {
+  margin-bottom: 20px;
+  padding: 10px;
+  &-title {
+    padding: 10px;
+  }
+  &-item {
+    padding: 0 5px;
+    margin-bottom: 10px;
+  }
+}
+</style>
